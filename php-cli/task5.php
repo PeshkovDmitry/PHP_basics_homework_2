dce2f434ab0c878d0e018fa3600cf75051073381
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-08-12 19:23:41 +0300

    Добавлены пустые файлы с текстом заданий для домашней работы 2

---
+++ php-cli/task5.php
@@ -0,0 +1,11 @@
+<?php
+
+// Задание 5
+
+// С помощью рекурсии организовать функцию возведения числа в степень. 
+// Формат: function power($val, $pow), где $val – заданное число, $pow – степень.
+
+
+
+
+// docker run --rm -v c:/Users/Дима/PHP/php-cli/:/cli php:8.2-cli php /cli/task5.php
