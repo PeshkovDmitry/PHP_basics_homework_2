55aadd9d9b5711ad8e3ac2f854666a0da5ad2933
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-08-12 19:46:05 +0300

    Решено задание 2

--- php-cli/task2.php
+++ php-cli/task2.php
@@ -7,4 +7,29 @@
 // В зависимости от переданного значения операции выполнить одну из арифметических операций 
 // (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
 
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case "add":
+            return $arg1 + $arg2;
+        case "sub":
+            return $arg1 - $arg2;    
+        case "mul":
+            return $arg1 * $arg2;     
+        case "div":
+            return $arg1 / $arg2;     
+        default:
+            return "Неизвестная операция";    
+    }
+}
+
+
+
+$a = 3;
+$b = 2;
+
+echo "a + b = {$a} + {$b} = " . mathOperation($a, $b, "add") . "\n\r";
+echo "a - b = {$a} - {$b} = " . mathOperation($a, $b, "sub") . "\n\r";
+echo "a * b = {$a} * {$b} = " . mathOperation($a, $b, "mul") . "\n\r";
+echo "a / b = {$a} / {$b} = " . mathOperation($a, $b, "div") . "\n\r";
+
 // docker run --rm -v ${pwd}/PHP/php-cli/:/cli php:8.2-cli php /cli/task2.php
